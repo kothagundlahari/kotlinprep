15d8d35d25198d2ed105665abab4e9a7ec35211e
hari krisha <hari.k.krishna4@gmail.com>
2018-04-15 01:01:53 -0400

    var vs val

--- src/DataTypes.kt
+++ src/DataTypes.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>) {
-    var name ="hari"
+    var name = "hari"
     var age = 23
     var dept = "IT"
-    println("name:"+name)
-    println("age:"+age)
-    println("dept:"+dept)
+    println("name:" + name)
+    println("age:" + age)
+    println("dept:" + dept)
 
 }
---
+++ src/var vs val.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var name = "hari"
+    name = "krishna"
+    val age = 23
+
+    println("name:"+name)
+    println("age"+age)
+}
